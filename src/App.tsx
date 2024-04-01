7cf23e1e436e6052e811810131fcd4582dde17c6
pre-Coded <princekr.it.21@gmail.com>
2024-04-01 18:13:13 +0530

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState<Number>(0)
+
+  return (
+    <p>Hello, world</p>
+  )
+}
+
+export default App
